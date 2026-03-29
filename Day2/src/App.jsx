38fc307081e82d9ebd5aff57a2477a0ca56e5b7a
Janani <23it021@kpriet.ac.in>
2026-03-29 15:33:15 +0530

    Create App.jsx

---
+++ Day2/src/App.jsx
@@ -0,0 +1,15 @@
+import UsersList from "./UsersList";
+import Conditional from "./Conditional";
+
+function App() {
+  return (
+    <div>
+      <h1>React Practice</h1>
+
+      <UsersList />
+      <Conditional />
+    </div>
+  );
+}
+
+export default App;
