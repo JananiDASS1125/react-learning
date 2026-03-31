dcda042d796bd97ade25edd142f0fbaa16b13273
Janani <23it021@kpriet.ac.in>
2026-03-31 15:19:13 +0530

    Create App.jsx

---
+++ Day4/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "./Counter";
+import EffectExample from "./EffectExample";
+
+function App() {
+  return (
+    <div>
+      <h1>React Hooks Practice</h1>
+
+      <Counter />
+      <EffectExample />
+    </div>
+  );
+}
+
+export default App;
