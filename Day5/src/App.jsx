6be7a889e49111e94918eb8c00aeaee1b971c2c6
Janani <23it021@kpriet.ac.in>
2026-04-01 13:54:11 +0530

    Create App.jsx

---
+++ Day5/src/App.jsx
@@ -0,0 +1,12 @@
+import FetchLocalData from "./FetchLocalData";
+
+function App() {
+  return (
+    <div>
+      <h1>Local JSON Server Data</h1>
+      <FetchLocalData />
+    </div>
+  );
+}
+
+export default App;
